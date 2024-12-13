b637d41c1f940b7f10433c9731ad981136fe2302
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2024-12-13 10:24:02 +0530

    Update config.php

--- config.php
+++ config.php
@@ -42,3 +42,48 @@
     }
  }
 ?>
+
+
+//update
+
+<?php 
+
+ header("Access-Control-Allow-Method:PUT");
+ header("Content-Type:application/json");
+
+ include("config.php");
+
+ $c1= new Config();
+
+
+ if($_SERVER["REQUEST_METHOD"]== "PUT")
+ {
+
+    $data = file_get_contents("php://input");
+    parse_str($data, $result);
+    $id = $result["id"];
+    $name = $result["name"];
+    $age = $result["age"];
+    $course = $result["course"];
+    $address = $result["address"];
+
+    $res=$c1->update($id, $name, $age, $course, $address);
+
+    if($res)
+    { 
+       $arr["msg"] = "Data Updated Successfully";
+    }
+    else
+    {
+        $arr["msg"] = "Data not Update";
+    }
+
+ }
+ else
+ {
+    $arr['err'] = "Only PUT type is allowed ";
+ }
+
+ echo json_encode($arr);
+
+?>
