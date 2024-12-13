9d0dcf1a9e45a99fa6fc6c5f686df54ddcd52df3
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2024-12-13 10:25:05 +0530

    Update config.php

--- config.php
+++ config.php
@@ -87,3 +87,43 @@
  echo json_encode($arr);
 
 ?>
+
+//delete
+
+<?php 
+
+ header("Access-Control-Allow-Method:DELETE");
+ header("Content-Type:application/json");
+
+ include("config.php");
+
+ $c1= new Config();
+
+
+ if($_SERVER["REQUEST_METHOD"]== "DELETE")
+ {
+
+    $data = file_get_contents("php://input");
+    parse_str($data, $result);
+    $id = $result["id"];
+
+    $res=$c1->delete($id);
+
+    if($res)
+    { 
+       $arr["msg"] = "Data deleted Successfully";
+    }
+    else
+    {
+        $arr["msg"] = "Data not deleted";
+    }
+
+ }
+ else
+ {
+    $arr['err'] = "Only DELETE type is allowed ";
+ }
+
+ echo json_encode($arr);
+
+?>
